7b69d47244963eb18ce9b4491a45f2d352edba91
RedWalters <jonathan.j.walters@Gmail.com>
2023-02-07 16:21:08 +0000

    Updating title to remove reference to dimension values

--- datasets/ONS-Prevalence-of-Domestic-Abuse-in-the-Last-Year-Among-Adults-Aged-16-to-59-by-Type-of-Abuse-and-Sex/main.py
+++ datasets/ONS-Prevalence-of-Domestic-Abuse-in-the-Last-Year-Among-Adults-Aged-16-to-59-by-Type-of-Abuse-and-Sex/main.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[63]:
+# In[94]:
 
 
 from gssutils import *
 
 
-# In[64]:
+# In[95]:
 
 
 scraper = Scraper(seed="info.json")
 scraper
 
 
-# In[65]:
+# In[96]:
 
 
 tabs = { tab: tab for tab in scraper.distributions[0].as_databaker() if tab.name in ['Table 3', 'Table 4']}
 
 
-# In[66]:
+# In[97]:
 
 
 for i in tabs:
     print(i.name)
 
 
-# In[67]:
+# In[98]:
 
 
 tidied_sheets = []
@@ -89,7 +89,7 @@ for tab in tabs:
 df
 
 
-# In[87]:
+# In[99]:
 
 
 import re
@@ -139,7 +139,7 @@ df['Value'] = df['Value'].round(1)
 df
 
 
-# In[88]:
+# In[100]:
 
 
 from IPython.core.display import HTML
@@ -150,12 +150,12 @@ for col in df:
         display(df[col].cat.categories)
 
 
-# In[89]:
+# In[101]:
 
 
 notes = """New questions were introduced into the survey from the year ending March 2013, and estimates from this year onwards are calculated using these new questions. Estimates for earlier years are calculated from the original questions with an adjustment applied to make them comparable to the new questions. From April 2017, the upper age limit for the self-completion module was increased to ask all respondents aged 16 to 74. Figures for 16 to 59 year olds only are presented in this table to allow comparisons to be made over a longer time period. A small change to the weighting procedure was made in 2019. This change is being applied going forward and was incorporated into all historic datasets. The effect of this change will only have a negligible impact on the estimates in this table and therefore historic data have not been re-calculated using the new weights, except for the year ending March 2018, where direct comparisons were previously made to the year ending March 2019. Estimates for the year ending March 2005 could not be re-calculated due to a manual adjustment which was applied to make the data comparable with the year ending 2013 onwards. More information can be found in footnote 3.	No data is available for the year ending March 2008 because comparable questions on any domestic abuse, any partner abuse and any family abuse were not included in that year. The sample size is lower for the years ending March 2011, March 2012 and March 2013 than for other years due to use of a split-sample experiment in these years. The sample size is lower for the years ending March 2018 and March 2019 due to use of a split-sample experiment. The sum of the overarching domestic abuse categories is not the sum of the sub-categories as some victims may be included in multiple categories as they can experience more than one type of abuse. The bases given are for any domestic abuse except for year ending March 2008 which is for partner abuse (non-sexual); the bases for the other measures presented will be similar."""
 scraper.dataset.description = notes
-scraper.dataset.title = 'Prevalence of Domestic Abuse in the Last Year Among Adults Aged 16 to 59, by Type of Abuse and Sex'
+scraper.dataset.title = 'Prevalence of Domestic Abuse'# in the Last Year Among Adults Aged 16 to 59, by Type of Abuse and Sex'
 
 df.to_csv('observations.csv', index=False)
 
