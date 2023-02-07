5f71faf1e4b9c99cd692d2b8f5e9e7556329416c
RedWalters <jonathan.j.walters@Gmail.com>
2023-02-07 15:53:04 +0000

    All all

--- datasets/ONS-Prevalence-of-Domestic-Abuse-in-the-Last-Year-Among-Adults-Aged-16-to-59-by-Type-of-Abuse-and-Sex/main.py
+++ datasets/ONS-Prevalence-of-Domestic-Abuse-in-the-Last-Year-Among-Adults-Aged-16-to-59-by-Type-of-Abuse-and-Sex/main.py
@@ -89,7 +89,7 @@ for tab in tabs:
 df
 
 
-# In[83]:
+# In[85]:
 
 
 import re
@@ -116,7 +116,7 @@ df['Abuse Category'] = df['Abuse Category'].str.strip().str.replace('  ' , ' ')
 df['Type of Abuse'] = df['Type of Abuse'].str.strip().str.replace('  ' , ' ')
 
 df = df.replace({'Sex' : {'All' : 't', 'Men' : 'm', 'Women' : 'f'},
-                'Type of Abuse' : {'Unweighted base - number of adults' : 'All',
+                'Type of Abuse' : {'Unweighted base - number of adults' : 'all',
                                    'Sexual assault by rape or penetration  by a partner' : 'Sexual assault by rape or penetration by a partner'},
                 'DATAMARKER' : {'[x]' : 'not-available',
                                 '[c]' : 'suppressed',
@@ -139,7 +139,7 @@ df['Value'] = df['Value'].round(1)
 df
 
 
-# In[84]:
+# In[86]:
 
 
 from IPython.core.display import HTML
